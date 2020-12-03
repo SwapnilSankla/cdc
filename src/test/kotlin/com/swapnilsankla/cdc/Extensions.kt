6441692cad9cef353a698f04fe6d0040e450d95a
Swapnil Sankla <swapnil.sankla@gmail.com>
2020-12-03 12:38:04 +0530

    Remove hard coded pact.provier.version and replace it with app version

--- src/test/kotlin/com/swapnilsankla/cdc/Extensions.kt
+++ src/test/kotlin/com/swapnilsankla/cdc/Extensions.kt
@@ -2,10 +2,17 @@ package com.swapnilsankla.cdc
 
 import org.junit.jupiter.api.extension.BeforeAllCallback
 import org.junit.jupiter.api.extension.ExtensionContext
+import org.springframework.core.io.support.PropertiesLoaderUtils
 
 class PactVerificationResultExtension: BeforeAllCallback {
     override fun beforeAll(context: ExtensionContext?) {
         System.setProperty("pact.verifier.publishResults", "true")
-        System.setProperty("pact.provider.version", "0.0.3")
+        System.setProperty("pact.provider.version", getVersion())
+    }
+
+    private fun getVersion(): String {
+        return PropertiesLoaderUtils
+            .loadAllProperties("application.yaml")
+            .getProperty("app.version")
     }
 }
